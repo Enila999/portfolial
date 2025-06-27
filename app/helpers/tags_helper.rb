736078a2e81cabfbd26469a095f392b2b1d831dd
Aline GOMES LOPES <aline.gl21@gmail.com>
2025-06-27 22:01:20 +0200

    redesign banner & new filter index

---
+++ app/helpers/tags_helper.rb
@@ -0,0 +1,12 @@
+module TagsHelper
+  def category_tags_map
+  {
+    "figma" => ["Figma", "Canva", "Mockup", "UX/UI", "Charte graphique", "Maquette", "Identité de marque"],
+    "adobe" => ["Photoshop", "Illustrator", "After Effect", "Premier Pro"],
+    "cms" => ["WordPress", "Wix", "Webflow"],
+    "site" => ["HTML", "CSS", "JavaScript", "Ruby on Rails", "Site internet"],
+    "maquette" => ["Affiche", "Bannière", "Panneau", "Flocage", "Logo"],
+    "réseaux sociaux" => ["Réseaux sociaux", "Communication", "Vidéo", "SEO", "Référencement", "Accompagnement"]
+  }
+  end
+end
