3d65c1c0ddb338a9a323f2237efdca77e3013674
Aline GOMES LOPES <aline.gl21@gmail.com>
2025-08-14 20:58:58 +0200

    fixing images

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,19 +5,3 @@ import { application } from "controllers/application"
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
-
-import NavbarController from "./navbar_controller"
-application.register("navbar", NavbarController)
-
-import AnimateTextController from "./animate_text_controller"
-application.register("animate-text", AnimateTextController)
-
-import HoverTextController from "./hover_text_home_controller"
-application.register("hover-text", HoverTextController)
-
-import LightboxController from "./lightbox_controller"
-application.register("lightbox", LightboxController)
